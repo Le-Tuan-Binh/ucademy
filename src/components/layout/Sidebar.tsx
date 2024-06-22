a554ef0424a72bcf29ad88a461791c01ff96d21d
Lê Tuấn Bình <TBinTB0003@gmail.com>
2024-06-23 03:06:22 +0700

    Deploy webhook

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -13,7 +13,7 @@ const SideBar = () => {
 	return (
 		<div className="p-5 border-r border-r-gray-200 dark:border-opacity-10 bg-white dark:bg-grayDarker flex flex-col">
 			<a href="/" className="font-bold text-3xl inline-block mb-5">
-				<span className="text-primary">U</span>
+				<span className="text-primary text-4xl">U</span>
 				cademy
 			</a>
 			<ul className="flex flex-col gap-3">
--- src/database/user.model.ts
+++ src/database/user.model.ts
@@ -5,9 +5,9 @@ export interface IUser extends Document {
 	clerkId: string;
 	name: string;
 	username: string;
-	email_address: string;
+	email: string;
 	avatar: string;
-	createdAt: Date;
+	created_at: Date;
 	status: EUserStatus;
 	role: EUserRole;
 	courses: Schema.Types.ObjectId[];
@@ -25,7 +25,7 @@ const userSchema = new Schema<IUser>({
 		unique: true,
 		required: true,
 	},
-	email_address: {
+	email: {
 		type: String,
 		unique: true,
 		required: true,
@@ -39,7 +39,7 @@ const userSchema = new Schema<IUser>({
 			ref: "Course",
 		},
 	],
-	createdAt: {
+	created_at: {
 		type: Date,
 		default: Date.now(),
 	},
