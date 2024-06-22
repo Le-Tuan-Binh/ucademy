47d3301b7472350eeff78801e475164c22a43e1a
Lê Tuấn Bình <TBinTB0003@gmail.com>
2024-06-23 03:09:31 +0700

    Change page.tsx

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -5,12 +5,6 @@ import createUser from "@/lib/actions/user.actions";
 import React from "react";
 
 const page = async () => {
-	// const user = await createUser({
-	// 	clerkId: "user_2iFGmKamJA0Rmcr5kZJo8LQz7lh",
-	// 	email_address: "TBinTB0003@gmail.com",
-	// 	name: "Lê Tuấn Bình",
-	// 	username: "ltbinh_user",
-	// });
 	return (
 		<div>
 			<Heading>Khám phá</Heading>
