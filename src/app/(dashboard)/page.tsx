e395a11dd3ac713d1194e5d661b1686e1d9133f7
Lê Tuấn Bình <TBinTB0003@gmail.com>
2024-06-23 02:08:52 +0700

    Update the connect mongodb to add new schema Users and Document

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,16 @@
 import { CourseGrid } from "@/components/common";
 import CourseItem from "@/components/course/CourseItem";
 import Heading from "@/components/typography/Heading";
+import createUser from "@/lib/actions/user.actions";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+	// const user = await createUser({
+	// 	clerkId: "user_2iFGmKamJA0Rmcr5kZJo8LQz7lh",
+	// 	email_address: "TBinTB0003@gmail.com",
+	// 	name: "Lê Tuấn Bình",
+	// 	username: "ltbinh_user",
+	// });
 	return (
 		<div>
 			<Heading>Khám phá</Heading>
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,12 +1,15 @@
-"user server";
+"use server";
 
 import User, { IUser } from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import { TCreateUserParams } from "@/types";
 
-export default async function createUser(params: IUser) {
+export default async function createUser(params: TCreateUserParams) {
 	try {
 		connectToDatabase();
-		const newUser = await User.create(params);
+		const newUser: TCreateUserParams = await User.create(params);
 		return newUser;
-	} catch (err) {}
+	} catch (err) {
+		console.log(err);
+	}
 }
