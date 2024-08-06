c9daab34f021f3bd7825f1d0b5cdd1469268ca4f
Lê Tuấn Bình <TBinTB0003@gmail.com>
2024-08-06 08:48:12 +0700

    Fix bug when deploy

--- src/app/(dashboard)/[course]/lesson/LessonWrapper.tsx
+++ src/app/(dashboard)/[course]/lesson/LessonWrapper.tsx
@@ -3,12 +3,12 @@ import useGlobalStore from "@/store";
 import React from "react";
 
 const LessonWrapper = ({ children }: { children: React.ReactNode }) => {
-	const { expandedPlayer, setExpandedPlayer } = useGlobalStore();
+	const { expandedMedia, setExpandedMedia } = useGlobalStore();
 	return (
 		<div
 			className="block xl:grid xl:grid-cols-[minmax(0,2fr),minmax(0,1fr)] gap-10 min-h-screen items-start"
 			style={{
-				display: expandedPlayer ? "block" : "grid",
+				display: expandedMedia ? "block" : "grid",
 			}}
 		>
 			{children}
