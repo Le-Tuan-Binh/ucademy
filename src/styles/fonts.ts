0c7068309a997c573e32a1f64335cf88ef2f4df0
Lê Tuấn Bình <TBinTB0003gmail.com>
2025-01-30 10:09:53 +0700

    Create the font config for project

---
+++ src/styles/fonts.ts
@@ -0,0 +1,15 @@
+import { Manrope, Roboto } from "next/font/google";
+
+const manrope = Manrope({
+	subsets: ["latin"],
+	weight: ["400", "500", "700"],
+	variable: "--font-manrope",
+});
+
+const roboto = Roboto({
+	subsets: ["latin"],
+	weight: ["400", "500", "700"],
+	variable: "--font-roboto",
+});
+
+export { manrope, roboto };
